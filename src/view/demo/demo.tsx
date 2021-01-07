866bbe1bfa3d6e6c681ba7d00f09cec0b99fe53c
mazhihong_v <mazhihong_v@didichuxing.com>
2021-01-07 20:48:17 +0800

    添加列表测试

--- src/view/demo/demo.tsx
+++ src/view/demo/demo.tsx
@@ -11,17 +11,19 @@ interface IProps extends IAPagePage<DemoModel>{
 export default class Demo extends APage<IProps, DemoModel> {
     private timer: any;
     private handleAdd = (): void => {
-        this.handleStop();
-        this.timer = setInterval(()=>{
-            this.model.setNum(this.model.num + 1);
-            // this.model.num = this.model.num + 1;
-        }, 0);
+        // this.handleStop();
+        // this.timer = setInterval(()=>{
+        //     this.model.setNum(this.model.num + 1);
+        //     // this.model.num = this.model.num + 1;
+        // }, 0);
+        const list = this.model.list;
+        this.model.push({
+            id: list.length,
+            name: Math.random().toString(32).substr(0, 8)
+        })
     }
     private handleStop = () => {
-        if (this.timer) {
-            clearInterval(this.timer);
-            this.timer = null;
-        }
+        this.model.shift();
     }
     public componentDidUpdate(prevProps: IProps, prevState: any) {
         super.componentDidUpdate(prevProps,prevState);
@@ -30,6 +32,17 @@ export default class Demo extends APage<IProps, DemoModel> {
     public render() {
         return (
             <div>
+                <ul>
+                    {
+                        this.model.list.map((item, index)=>{
+                            return (
+                                <li key={ index }>
+                                    {item.name}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
                 <p>{ this.model.num }</p>
                 <button onClick={ this.handleAdd }>START</button>
                 <button onClick={ this.handleStop }>STOP</button>
--- src/view/demo/demoModel.ts
+++ src/view/demo/demoModel.ts
@@ -2,6 +2,14 @@ import { action, makeObservable, observable } from 'mobx';
 
 export default class DemoModel {
     @observable public num: number = 1;
+    @observable public list: Array<{
+        id: number;
+        name: string;
+    }> = [
+        {id: 0, name: '0'},
+        {id: 1, name: '1'},
+        {id: 2, name: '2'},
+    ];
 
     constructor() {
         makeObservable(this);
@@ -10,6 +18,14 @@ export default class DemoModel {
     @action
     public setNum(val: number): void {
         this.num = val;
+    }
+    @action
+    public push(item: any){
+        this.list.unshift(item);
+    }
+    @action
+    public shift(){
+        this.list.shift();
     }
 
     public getNum(): number {
